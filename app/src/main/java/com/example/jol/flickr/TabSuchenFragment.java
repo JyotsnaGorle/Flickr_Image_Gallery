561869e919850b2b4a5ff4fd9b49a3883843b447
Jyotsna Gorle <jyotsnagorle@gmail.com>
2019-01-17 10:48:19 +0100

    add search bar and switches

--- app/src/main/java/com/example/jol/flickr/TabSuchenFragment.java
+++ app/src/main/java/com/example/jol/flickr/TabSuchenFragment.java
@@ -1,17 +1,22 @@
 package com.example.jol.flickr;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TabSuchenFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suchen_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_suchen_tab, container, false);
+        TextView title = view.findViewById(R.id.title);
+        title.setText(getString(R.string.suche_title));
+        return view;
     }
 
 }
